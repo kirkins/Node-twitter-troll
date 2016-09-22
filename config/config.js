5f494683c4b7233e45adb7d245ef1826d4213321
Philip Kirkbride <kirkins@gmail.com>
2016-09-22 19:40:53 -0400

    cleaned up project and changed focus to twitter trolling

---
+++ config/config.js
@@ -0,0 +1,12 @@
+const config = {};
+// Fill these out with credentials from www.apps.twitter.com
+config.TWITTER_CONSUMER_KEY = 's4ONiRAc5I8ICwvnmPrYjGmJe';
+config.TWITTER_CONSUMER_SECRET = 'ibkJuvs6fnLTguYJqzJfJlUfDWm1V11krxdF7fFvnnxDbFJaD7';
+config.TWITTER_ACCESS_TOKEN = '36999372-sUn8fttS4L6644K5E4I1nBxBfbeJPalsyBUWv08NZ';
+config.TWITTER_ACCESS_TOKEN_SECRET = 'aeAexWNrC4ZtpaUoRXIOUoGqj46IDemamW75LAVTw8SM8';
+config.TWITTER_SEARCH_PHRASE = '@gnarly_karl OR Hillary Clinton';
+config.TWITTER_SEARCH_START_DATE = '2009-06-01'
+config.TWITTER_LAST_REPLIED = 743205419682775040; // don't search tweets older than last replied
+config.TWEET_FREQUENCY = 10; // tweet every minutes
+
+module.exports = config;
--- twitter.js
+++ twitter.js
@@ -1,26 +1,16 @@
-#!/usr/bin/env node
+// See config file for values needed on setup
+const config = require('./config/config');
+const Twit = require('twit');
 
-// Fill these out with credentials from apps.twitter.com
-var TWITTER_CONSUMER_KEY = '';
-var TWITTER_CONSUMER_SECRET = '';
-var TWITTER_ACCESS_TOKEN = '';
-var TWITTER_ACCESS_TOKEN_SECRET = '';
-
-var TWITTER_SEARCH_PHRASE = '@ph___p'; // make bots handle to respond to incoming tweets
-var TWITTER_LAST_REPLIED = 1; // change to skip all before tweet ID
-var TWEET_FREQUENCY = 7; // check for tweets every X minutes, tweeting too fast can get app restricted
-
-var Twit = require('twit');
-
-var twitterApi = new Twit({
-  consumer_key: TWITTER_CONSUMER_KEY,
-  consumer_secret: TWITTER_CONSUMER_SECRET,
-  access_token: TWITTER_ACCESS_TOKEN,
-  access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
+var Bot = new Twit({
+  consumer_key: config.TWITTER_CONSUMER_KEY,
+  consumer_secret: config.TWITTER_CONSUMER_SECRET,
+  access_token: config.TWITTER_ACCESS_TOKEN,
+  access_token_secret: config.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 
-var pkg = require('./package.json');
+const pkg = require('./package.json');
 var Surly = require('./src/Surly');
 var conf = require('rc')('surly', {
   brain: '',
@@ -35,40 +25,41 @@ var options = {
   version: conf.version,
 }
 
-var bot = new Surly({
+var sur = new Surly({
   brain: options.brain
 });
 
-console.log('responding to ' + TWITTER_SEARCH_PHRASE + ' every '  + TWEET_FREQUENCY + ' minutes');
+console.log('Surly started will respond to a tweet every ' + config.TWEET_FREQUENCY + ' minutes');
 
 function BotReply() {
 
-  var query = { // more options https://dev.twitter.com/rest/reference/get/search/tweets
-    q: TWITTER_SEARCH_PHRASE,
+  var query = {
+    q: config.TWITTER_SEARCH_PHRASE,
     count: 100,
     result_type: "recent",
-    since_id: TWITTER_LAST_REPLIED
+    since_id: config.TWITTER_LAST_REPLIED
   }
 
-  twitterApi.get('search/tweets', query, BotGotLatestTweet);
+  Bot.get('search/tweets', query, BotGotLatestTweet);
 
   function BotGotLatestTweet(error, data, response) {
     if (error) {
       console.log('Bot could not find latest tweet, : ' + error);
     } else {
-      var tweetQue = data.statuses.length - 1;
+      var tweetQue = data.statuses.length - 1; //start at last tweet in search to check if replied
       while (tweetQue > -1) {
-        if (data.statuses[tweetQue].text.substring(0, 2) != 'RT') { //if tweet is a RT skip
+        if (data.statuses[tweetQue].text.substring(0, 2) != 'RT') { //check if a RT
           var tweetId = data.statuses[tweetQue].id_str;
           var username = data.statuses[tweetQue].user.screen_name;
-          console.log(data.statuses[tweetQue].text);
-          // clean text for use with AIML
+          // Clean msg for urls and weird chars before aiml
+          // Unfortunately this means twitter names are stripped of @ and _
           var text = data.statuses[tweetQue].text
             .replace(/(?:https?|ftp):\/\/[\n\S]+/g, '')
-            .replace(/_/g, "unn")
-            .replace(/@/g, "att");
+            .replace(/[^\w\s]|_/g, "")
+            .replace(/\s+/g, " ");
+          console.log(text);
           respondTo(username, text, tweetId);
-	  return;
+	  return; // this makes only respond to one person at a time
         }
         tweetQue--;
       }
@@ -76,27 +67,29 @@ function BotReply() {
   }
 
   function respondTo(username, text, tweetId) {
-    bot.talk(text, function(err, response) {
-      TWITTER_LAST_REPLIED = tweetId;
+    sur.talk(text, function(err, response) {
+      config.TWITTER_LAST_REPLIED = tweetId;
       if (err) {
         console.log('error: ' + err);
       }
       else if (response) {
         var cleanText = response.toLowerCase();
-        cleanText = cleanText.replace(/unn/g, "_")
-        .replace(/att/g, "@");
+        cleanText = cleanText.replace(/yyy/g, "_")
+          .replace(/xxx/g, "@");
+
         console.log('Sending: ' + cleanText);
-        twitterApi.post('statuses/update', {
+        Bot.post('statuses/update', {
           status: '@' + username + ' ' + cleanText,
           in_reply_to_status_id: tweetId
         });
+        console.log('tweet sent');
         return;
       }
-        console.log('didn\'t return an error or response trying another tweet');
-        BotReply();
+      console.log('didn\'t return an error or response trying another tweet');
+      BotReply();
     });
   }
 
 }
 
-var timer = setInterval(BotReply, TWEET_FREQUENCY * 60 * 60 * 10);
+var timer = setInterval(BotReply, config.TWEET_FREQUENCY * 60 * 60 * 10);
