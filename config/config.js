ba0fe69ce12ad2c750fe41697f6ef151418ea45d
Philip Kirkbride <kirkins@gmail.com>
2016-09-23 18:29:27 -0400

    remove credentials

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 const config = {};
 // Fill these out with credentials from www.apps.twitter.com
-config.TWITTER_CONSUMER_KEY = 's4ONiRAc5I8ICwvnmPrYjGmJe';
-config.TWITTER_CONSUMER_SECRET = 'ibkJuvs6fnLTguYJqzJfJlUfDWm1V11krxdF7fFvnnxDbFJaD7';
-config.TWITTER_ACCESS_TOKEN = '36999372-sUn8fttS4L6644K5E4I1nBxBfbeJPalsyBUWv08NZ';
-config.TWITTER_ACCESS_TOKEN_SECRET = 'aeAexWNrC4ZtpaUoRXIOUoGqj46IDemamW75LAVTw8SM8';
+config.TWITTER_CONSUMER_KEY = '';
+config.TWITTER_CONSUMER_SECRET = '';
+config.TWITTER_ACCESS_TOKEN = '';
+config.TWITTER_ACCESS_TOKEN_SECRET = '';
 config.TWITTER_SEARCH_PHRASE = '@gnarly_karl OR Hillary Clinton';
 config.TWITTER_SEARCH_START_DATE = '2009-06-01'
 config.TWITTER_LAST_REPLIED = 743205419682775040; // don't search tweets older than last replied
